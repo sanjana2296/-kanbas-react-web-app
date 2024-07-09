f9043284f10f7195359c8b1d2c1f0dbf0f76711f
sanjana2296 <sanjana.poojary22@gmail.com>
2024-07-08 21:57:44 -0400

    Account Calender Inbox created

---
+++ src/Kanbas/Calender/index.tsx
@@ -0,0 +1,7 @@
+export default function Calender() {
+    return (
+      <div id="wd-calender">
+        <h2>Calender</h2>
+      </div>
+  );}
+  
--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -2,6 +2,9 @@ import Dashboard from "./Dashboard";
 import KanbasNavigation from "./Navigation";
 import { Routes, Route, Navigate } from "react-router";
 import Courses from "./Courses";
+import Account from "./Account";
+import Calender from "./Calender";
+import Inbox from "./Inbox";
 export default function Kanbas() {
     return (
       <div id="wd-kanbas">
@@ -13,8 +16,11 @@ export default function Kanbas() {
           <td valign="top">
           <Routes>
               <Route path="/" element={<Navigate to="Dashboard" />} />
+              <Route path="Account" element={<Account/>} />
               <Route path="Dashboard" element={<Dashboard />} />
               <Route path="Courses/:id/*" element={<Courses />} />
+              <Route path="Calendar" element={<Calender/>} />
+              <Route path="Inbox" element={<Inbox/>} />
             </Routes>
 
           </td>

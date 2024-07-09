0b2c547b176e53d1243fbfac9d498cffa354d33b
sanjana2296 <sanjana.poojary22@gmail.com>
2024-07-08 21:42:32 -0400

    Course link

--- src/Kanbas/Navigation/index.tsx
+++ src/Kanbas/Navigation/index.tsx
@@ -4,7 +4,7 @@ export default function KanbasNavigation() {
         <li><a id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/">Northeastern</a></li>
         <li><a id="wd-account-link" href="#/Kanbas/Account">Account</a></li>
         <li><a id="wd-dashboard-link" href="#/Kanbas/Dashboard">Dashboard</a></li>
-        <li><a id="wd-course-link" href="#/Kanbas/Courses">Courses</a></li>
+        <li><a id="wd-course-link" href="#/Kanbas/Courses/1234/Home">Courses</a></li>
         <li><a id="wd-calendar-link" href="#/Kanbas/Calendar">Calendar</a></li>
         <li><a id="wd-inbox-link" href="#/Kanbas/Inbox">Inbox</a></li>
         <li><a id="wd-labs-link" href="#/Labs">Labs</a></li>

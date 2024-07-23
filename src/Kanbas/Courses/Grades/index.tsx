3f679287b4096c811f67a006f656c1a8512a7551
sanjana2296 <sanjana.poojary22@gmail.com>
2024-07-22 21:12:32 -0400

    Grades update

--- src/Kanbas/Courses/Grades/index.tsx
+++ src/Kanbas/Courses/Grades/index.tsx
@@ -2,12 +2,54 @@ import { IoSettingsSharp } from "react-icons/io5";
 import { FaFileExport } from "react-icons/fa6";
 import { TbFileArrowLeft } from "react-icons/tb";
 import { BsSearch } from "react-icons/bs";
-import { RiArrowDropDownLine } from "react-icons/ri";
 import { CiFilter } from "react-icons/ci";
+import { useParams } from "react-router-dom";
+import * as db from "../../Database";
+
 export default function Grades() {
+  const { cid } = useParams();
+  function getEnrollmentsForCourse(courseId: any) {
+    return db.enrollments.filter(
+      (enrollment) => enrollment.course === courseId
+    );
+  }
+  function getCourseDetails(courseId: any) {
+    return db.courses.find((course) => course._id === courseId);
+  }
+  const enrollments = getEnrollmentsForCourse(cid);
+  const courseDetails = getCourseDetails(cid);
+
+  const assignmentsList = db.assignments.filter(
+    (assignment) => assignment.course === cid
+  );
+
+  function getNameForUserId(uid: any) {
+    const user = db.users.find((user) => user._id === uid);
+    return user ? `${user.firstName} ${user.lastName}` : "Unknown";
+  }
+
+  function getGradeForAssignment(assignmentId: any, studentId: any) {
+    console.log(
+      "Looking for grade - Assignment ID:",
+      assignmentId,
+      "Student ID:",
+      studentId
+    );
+    const grade = db.grades.find(
+      (grade) =>
+        grade.assignment === assignmentId && grade.student === studentId
+    );
+    return grade ? `${grade.grade}%` : "Not Graded";
+  }
+
   return (
     <div>
-      Grades
+      <h2>Grades for {courseDetails ? courseDetails.name : "Course"}</h2>
+      <p>
+        {courseDetails
+          ? courseDetails.description
+          : "Course description not available."}
+      </p>
       <div className="d-flex justify-content-end p-3">
         <button className="btn btn-light me-2 px-3 py-2 text-dark">
           <FaFileExport /> Import
@@ -22,11 +64,6 @@ export default function Grades() {
       <div className="row">
         <div className="col-6">
           <h3>Student Names</h3>
-        </div>
-        <div className="col-6">
-          <h3>Assignment Names</h3>
-        </div>
-        <div className="col-6">
           <div className="input-group">
             <span className="input-group-text">
               <BsSearch />
@@ -38,8 +75,13 @@ export default function Grades() {
               id="wd-search-students"
             />
           </div>
+          <br />
+          <button className="btn btn-light me-2 px-3 py-2 text-dark">
+            <CiFilter /> Apply Filters
+          </button>
         </div>
         <div className="col-6">
+          <h3>Assignment Names</h3>
           <div className="input-group">
             <span className="input-group-text">
               <BsSearch />
@@ -54,84 +96,29 @@ export default function Grades() {
         </div>
       </div>
       <br />
-      <div>
-        <button className="btn btn-light me-2 px-3 py-2 text-dark">
-          <CiFilter /> Apply Filters
-        </button>
-      </div>
-      <br />
-      <br />
-      <br />
       <div className="table-responsive">
-        <table className="table table-bordered  table-striped">
+        <table className="table table-striped">
           <thead>
-            <tr className="bg-dark">
+            <tr>
               <th>Student Name</th>
-              <th>
-                A1 SETUP <br /> out of 100
-              </th>
-              <th>
-                A2 HTML <br /> out of 100
-              </th>
-              <th>
-                A3 CSS <br /> out of 100
-              </th>
-              <th>
-                A4 BOOTSTRAP <br />
-                out of 100
-              </th>
+              {assignmentsList.map((assignment) => (
+                <th key={assignment._id}>{assignment.title}</th>
+              ))}
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td style={{ color: "red" }}>Jane Adams</td>
-              <td>100%</td>
-              <td>96.67%</td>
-              <td>92.18%</td>
-              <td>66.22%</td>
-            </tr>
-            <tr>
-              <td style={{ color: "red" }}>Christina Allen</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-            </tr>
-            <tr>
-              <td style={{ color: "red" }}>Samreen Ansari</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-            </tr>
-            <tr>
-              <td style={{ color: "red" }}>Han Bao</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>88.03%</td>
-              <td>100%</td>
-            </tr>
-            <tr>
-              <td style={{ color: "red" }}>Mahi Sri Srinivas Bobbili</td>
-              <td>100%</td>
-              <td>96.67%</td>
-              <td>98.37%</td>
-              <td>100%</td>
-            </tr>
-            <tr>
-              <td style={{ color: "red" }}>Siran Cao</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>100%</td>
-            </tr>
-            <tr>
-              <td style={{ color: "red" }}>Sanjana Poojary</td>
-              <td>100%</td>
-              <td>100%</td>
-              <td>95%</td>
-              <td>100%</td>
-            </tr>
+            {enrollments.map((enrollment, enrollmentIndex) => (
+              <tr key={enrollmentIndex}>
+                <td style={{ color: "red" }}>
+                  {getNameForUserId(enrollment.user)}
+                </td>
+                {assignmentsList.map((assignment) => (
+                  <td key={assignment._id}>
+                    {getGradeForAssignment(assignment._id, enrollment.user)}
+                  </td>
+                ))}
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
